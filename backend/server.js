51036c48e1a41391bccc2fd2b2acfcfab6c2ed56
Rahul <97170490+03rahul007@users.noreply.github.com>
2024-09-23 15:20:00 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,20 @@ app.use(cors({
   credentials: true, // Allow cookies to be sent
 }));
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://a-1-vaxt.onrender.com");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  // If it's a preflight request, respond with 200
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
+  next();
+});
+
+
 // app.use(cors());
 // ---------Middlewares-----------
 app.use(express.static(path.join(__dirname, "public")));
